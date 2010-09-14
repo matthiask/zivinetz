78a5b526fb5e8474219dd3598c9a996e5446a3ba
Matthias Kestenholz <mk@spinlock.ch>
2010-09-14 17:15:16 +0200

    Move day calculation into an assignment member method

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
+from datetime import timedelta
+
 from django.contrib.auth.models import User
 from django.contrib.localflavor.ch import ch_states
 from django.db import models
 from django.utils.translation import ugettext_lazy as _, ugettext
-
-
 
 
 class ScopeStatement(models.Model):
@@ -181,6 +181,102 @@ class Assignment(models.Model):
     @property
     def determine_date_until(self):
         return self.date_until_extension or self.date_until
+
+
+    def calculate_days(self):
+        day = self.date_from
+        until = self.determine_date_until
+        one_day = timedelta(days=1)
+
+        public_holidays = PublicHoliday.objects.filter(
+            date__range=(day, until)).values_list('date', flat=True)
+        company_holidays = list(CompanyHoliday.objects.filter(
+            date_from__lte=until,
+            date_until__gte=day))
+
+        vacation_days = 0
+        assignment_days = (self.date_until - self.date_from).days + 1 # +1 because the range is inclusive
+
+        if assignment_days >= 180:
+            # TODO 30 days isn't exactly one month... (see ZDV Art. 72)
+            vacation_days = 8 + (assignment_days - 180) / 30 * 2
+
+        days = {
+            'assignment_days': assignment_days,
+            'vacation_days': vacation_days,
+
+            'company_holidays': 0,
+            'public_holidays_during_company_holidays': 0,
+            'public_holidays_outside_company_holidays': 0,
+
+            'vacation_days_during_company_holidays': 0,
+
+            'freely_definable_vacation_days': vacation_days,
+            'working_days': 0,
+
+            'countable_days': 0,
+            'forced_leave_days': 0, # days which aren't countable and are forced upon the drudge
+            }
+
+        def pop_company_holiday():
+            try:
+                return company_holidays.pop(0)
+            except IndexError:
+                return None
+
+        company_holiday = pop_company_holiday()
+
+        while day <= until:
+            is_weekend = day.weekday() in (5, 6)
+            is_public_holiday = day in public_holidays
+            is_company_holiday = company_holiday and company_holiday.is_contained(day)
+
+            if is_company_holiday:
+                days['company_holidays'] += 1
+
+                if is_public_holiday:
+                    # At least we have public holidays too.
+                    days['public_holidays_during_company_holidays'] += 1
+                    days['countable_days'] += 1
+                else:
+                    if is_weekend:
+                        # We were lucky once again.
+                        days['countable_days'] += 1
+                    else:
+                        # Oh no, company holiday and neither public holiday nor
+                        # weekend. Now the draconian regime of the swiss
+                        # administration comes to full power.
+
+                        if days['freely_definable_vacation_days']:
+                            # Vacations need to be taken during public holidays
+                            # if possible at all. Unfortunately for drudges.
+                            days['freely_definable_vacation_days'] -= 1
+
+                            # At least they are countable towards assignment total.-
+                            days['countable_days'] += 1
+                        else:
+                            # Damn. No vacation days left (if there were any in the
+                            # beginning. The drudge has to pause his assignment for
+                            # this time.
+                            days['forced_leave_days'] += 1
+
+            else:
+                # No company holiday... business as usual, maybe.
+                days['countable_days'] += 1 # Nice!
+
+                if not (is_public_holiday or is_weekend):
+                    # Hard beer-drinking and pickaxing action.
+                    days['working_days'] += 1
+
+
+            day += one_day
+
+            # Fetch new company holiday once the old one starts smelling funny.
+            if company_holiday and company_holiday.date_until < day:
+                company_holiday = pop_company_holiday()
+
+        return days
+
 
 
 class ExpenseReport(models.Model):
