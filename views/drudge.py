8d0c3e9a9328645971e5a7dfaa3d2b685361e231
Matthias Kestenholz <mk@spinlock.ch>
2011-05-10 14:17:17 +0200

    Allow creating assignments and waitlist entries from drudge dashboard

--- views/drudge.py
+++ views/drudge.py
@@ -7,21 +7,73 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.utils.translation import ugettext as _
 
-from zivinetz.models import Drudge, ExpenseReport
+from towel.forms import stripped_formfield_callback
+
+from zivinetz.models import Drudge, ExpenseReport, Assignment, WaitList
 from zivinetz.views.decorators import drudge_required
+
+
+class AssignmentForm(forms.ModelForm):
+    formfield_callback = stripped_formfield_callback
+
+    class Meta:
+        model = Assignment
+        exclude = ('drudge', 'created', 'status', 'date_until_extension')
+
+
+class WaitListForm(forms.ModelForm):
+    formfield_callback = stripped_formfield_callback
+
+    class Meta:
+        model = WaitList
+        exclude = ('drudge', 'created')
 
 
 @drudge_required
 def dashboard(request, drudge):
+
+    aform_initial = {
+        'regional_office': drudge.regional_office_id,
+        }
+
+    aform = AssignmentForm(initial=aform_initial)
+    wform = WaitListForm()
+
+    if request.method == 'POST':
+        if 'assignment' in request.POST:
+            aform = AssignmentForm(request.POST)
+            if aform.is_valid():
+                assignment = aform.save(commit=False)
+                assignment.drudge = drudge
+                assignment.save()
+                messages.success(request, _('Successfully saved new assignment.'))
+
+                return HttpResponseRedirect('.')
+
+        if 'waitlist' in request.POST:
+            wform = WaitListForm(request.POST)
+            if wform.is_valid():
+                waitlist = wform.save(commit=False)
+                waitlist.drudge = drudge
+                waitlist.save()
+                messages.success(request, _('Successfully saved new waitlist entry.'))
+
+                return HttpResponseRedirect('.')
+
     return render(request, 'zivinetz/drudge_dashboard.html', {
         'drudge': drudge,
 
+        'assignment_form': aform,
+        'waitlist_form': wform,
+
         'assignments': drudge.assignments.order_by('-date_from'),
         'expense_reports': ExpenseReport.objects.filter(assignment__drudge=drudge).order_by('-date_from'),
+        'waitlist': WaitList.objects.filter(drudge=drudge),
         })
 
 
 class UserForm(forms.ModelForm):
+    formfield_callback = stripped_formfield_callback
     error_css_class = 'error'
     required_css_class = 'required'
 
@@ -34,6 +86,7 @@ class UserForm(forms.ModelForm):
 
 
 class DrudgeForm(forms.ModelForm):
+    formfield_callback = stripped_formfield_callback
     error_css_class = 'error'
     required_css_class = 'required'
 
@@ -66,8 +119,6 @@ def profile(request):
     else:
         form = DrudgeForm(instance=drudge)
         form2 = UserForm(instance=request.user)
-
-        print form2
 
     return render(request, 'zivinetz/drudge_profile.html', {
         'object': drudge,
