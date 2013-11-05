3d817900c3bcdfd2e1bd650c6762a1efa7729dfe
Matthias Kestenholz <mk@spinlock.ch>
2013-11-05 17:26:53 +0100

    Search form collapsement

--- zivinetz/static/zivinetz/zivinetz.js
+++ zivinetz/static/zivinetz/zivinetz.js
@@ -1,38 +1,83 @@
-$(function() {
-    if ($.datepicker) {
-        $.datepicker.setDefaults({
-            changeYear: true,
-            changeMonth: true,
-            dateFormat: 'dd.mm.yy',
-            firstDay: 1,
-            yearRange: 'c-40:c+40'
-        });
+/* batch form */
+(function($) {
+    $(document.body).on('click', '[data-batch-action]', function() {
+        var input = $('input[name="batch-action"]');
+        input.val($(this).data('batch-action'));
+        input.parents('form').submit();
+    });
 
-        $('input.dateinput').datepicker();
-    }
+    $(document.body).on('click', 'input.batch:checkbox', function() {
+        var cb = $(this);
+        if (cb.prop('value') == 'on') {
+            cb.closest('table').find('tbody input.batch:checkbox').prop(
+                'checked', cb.prop('checked'));
+        } else {
+            cb.closest('table').find('thead input.batch:checkbox').prop(
+                'checked', false);
+        }
+    });
 
-    var searchbox = $('.searchbox');
-    var query_input = searchbox.find('input[name=query]');
-    if(searchbox.hasClass('searching')) {
-        query_input.focus();
-    } else {
-        searchbox.find('p').hide();
-        query_input.focus(function(){
-            searchbox.find('p').show();
-        }).click(function(){
-            searchbox.find('p').show();
-        }).keyup(function(event){
-            if(event.keyCode==27)
-                searchbox.find('p').hide();
-        });
-    }
 
     $('.objects thead input[type=checkbox]').bind('change', function() {
         var cbs = $('.objects tbody input[type=checkbox]');
-        if (this.checked) {
-            cbs.attr('checked', 'checked');
+        if ($(this).prop('checked')) {
+            cbs.prop('checked', 'checked');
         } else {
             cbs.removeAttr('checked');
         }
     });
-});
+
+})(jQuery);
+
+/* search form */
+(function($) {
+    var form = $('.form-search'),
+        panel = form.find('.panel');
+
+    if (!panel.length)
+        return;
+
+    $('.form-search').on('click', '[data-toggle="form"]', function() {
+        panel.toggle();
+    });
+    // give the layout engine some time to position everything
+    // before hiding the panel.
+    setTimeout(function() {
+        panel.hide();
+        panel.css({opacity: 1});
+    }, 10);
+})(jQuery);
+
+/* editLive */
+(function($) {
+    $(document.body).on('editLive', function(event, elem) {
+        if (elem.is(':checkbox'))  /* TODO NOT text input. selects! */
+            elem = elem.parent();
+
+        elem.addClass('editlive-updated');
+        setTimeout(function() {
+            elem.removeClass('editlive-updated');
+        }, 2000);
+    });
+})(jQuery);
+
+
+/* various tweaks */
+(function($) {
+    Foundation.libs.reveal.settings.animation = 'fade';
+
+    $(document).foundation();
+    $(document).towelFoundation();
+
+    $(document.body).append('<div id="spinner"></div>');
+    var spinner = $('#spinner');
+    spinner.bind('ajaxSend', function(evt, jqxhr, settings) {
+        if (settings.type == 'POST') {
+            spinner.html('Saving...').show();
+        } else {
+            spinner.html('Loading...').show();
+        }
+    }).bind('ajaxComplete', function() {
+        spinner.hide();
+    });
+})(jQuery);
