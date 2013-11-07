ab27a55a02dcdf700e3e5e307ad89789ef53bc3f
Matthias Kestenholz <mk@spinlock.ch>
2013-11-07 08:31:02 +0100

    Stop relying on localflavor

--- zivinetz/models.py
+++ zivinetz/models.py
@@ -5,10 +5,39 @@ from towel.managers import SearchManager
 from towel.modelview import ModelViewURLs
 
 from django.contrib.auth.models import User
-from django.contrib.localflavor.ch import ch_states
 from django.db import models
 from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _, ugettext
+
+
+STATE_CHOICES = (
+    ('AG', _('Aargau')),
+    ('AI', _('Appenzell Innerrhoden')),
+    ('AR', _('Appenzell Ausserrhoden')),
+    ('BS', _('Basel-Stadt')),
+    ('BL', _('Basel-Land')),
+    ('BE', _('Berne')),
+    ('FR', _('Fribourg')),
+    ('GE', _('Geneva')),
+    ('GL', _('Glarus')),
+    ('GR', _('Graubuenden')),
+    ('JU', _('Jura')),
+    ('LU', _('Lucerne')),
+    ('NE', _('Neuchatel')),
+    ('NW', _('Nidwalden')),
+    ('OW', _('Obwalden')),
+    ('SH', _('Schaffhausen')),
+    ('SZ', _('Schwyz')),
+    ('SO', _('Solothurn')),
+    ('SG', _('St. Gallen')),
+    ('TG', _('Thurgau')),
+    ('TI', _('Ticino')),
+    ('UR', _('Uri')),
+    ('VS', _('Valais')),
+    ('VD', _('Vaud')),
+    ('ZG', _('Zug')),
+    ('ZH', _('Zurich'))
+)
 
 
 class ScopeStatement(models.Model):
@@ -269,7 +298,7 @@ class DrudgeManager(SearchManager):
 
 
 class Drudge(models.Model):
-    STATES = [state[0] for state in ch_states.STATE_CHOICES]
+    STATES = [state[0] for state in STATE_CHOICES]
     STATE_CHOICES = zip(STATES, STATES)
 
     MOTOR_SAW_COURSE_CHOICES = (
