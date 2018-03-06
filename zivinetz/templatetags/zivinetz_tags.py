7a0df66a83ba228fb0feee2f44f26b86634c468f
Matthias Kestenholz <mk@feinheit.ch>
2018-03-06 17:15:27 +0100

    assignment_tag is gone

--- zivinetz/templatetags/zivinetz_tags.py
+++ zivinetz/templatetags/zivinetz_tags.py
@@ -6,6 +6,6 @@ from zivinetz.models import JobReferenceTemplate
 register = template.Library()
 
 
-@register.assignment_tag
+@register.simple_tag
 def job_reference_templates():
     return JobReferenceTemplate.objects.all()
