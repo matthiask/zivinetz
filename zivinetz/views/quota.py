09607ee80bb90df935703f25abdd13f50d6994a6
Matthias Kestenholz <mk@feinheit.ch>
2024-11-22 17:49:32 +0100

    Fix quota errors

--- zivinetz/views/quota.py
+++ zivinetz/views/quota.py
@@ -16,7 +16,7 @@ def yield_dates(start, step, count):
 
 
 class QuotaForm(forms.Form):
-    quota = forms.IntegerField(label=gettext_lazy("quota"), required=False)
+    quota = forms.IntegerField(label=gettext_lazy("quota"), required=False, min_value=0, max_value=100)
 
 
 @staff_member_required
